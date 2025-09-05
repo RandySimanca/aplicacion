37468c724f3107a382643b4ae91565028b96eb29
RandySimanca <randysimancamercado@gmail.com>
2025-09-04 21:56:54 -0500

    Add files via upload

---
+++ routes/datosPersonales.js
@@ -0,0 +1,26 @@
+//backend/routes/datosPersonales.js
+
+import express from "express";
+import { crearDatosPersonales } from "../controllers/datosPersonalesControllers.js";
+import { obtenerDatosPersonales } from "../controllers/datosPersonalesControllers.js";
+import { actualizarDatosPersonales } from "../controllers/datosPersonalesControllers.js";
+import auth from "../middlewares/auth.js";
+import verificarJWT from "../middlewares/verificarJWT.js";
+
+
+
+const router = express.Router();
+
+// Ruta de test (¡útil para verificar que se monte bien!)
+router.get("/test", (req, res) => {
+  res.send("🚀 Ruta de datos-personales activa");
+});
+
+// POST principal
+router.post("/datos-personales", verificarJWT, crearDatosPersonales);
+router.get("/datos-personales", verificarJWT, obtenerDatosPersonales);
+router.put("/datos-personales", verificarJWT, actualizarDatosPersonales);
+
+
+export default router;
+
