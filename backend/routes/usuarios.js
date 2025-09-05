ad8a58ce3e7898216f0beb0301b0181816942985
RandySimanca <randysimancamercado@gmail.com>
2025-09-04 21:53:16 -0500

    Delete backend directory

--- backend/routes/usuarios.js
+++
@@ -1,9 +0,0 @@
-// routes/usuarios.js
-import express from 'express';
-import { registrarUsuario }  from '../controllers/authController.js';
-
-const router = express.Router();
-
-router.post('/', registrarUsuario);
-
-export default router;
