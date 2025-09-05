6da534be5d89ad91287bde7f68b2110697fbab18
RandySimanca <randysimancamercado@gmail.com>
2025-09-04 23:06:58 -0500

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,15 @@
 //src/api.js
-import axios from 'axios';
+const api = axios.create({
+  baseURL: process.env.VUE_APP_API_URL,
+  withCredentials: true,
+});
+
+/**import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'http://localhost:3000/api',
   withCredentials: true,
 });
 
-export default api;
+export default api;*/
+
