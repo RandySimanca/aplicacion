757de4c5d3430771a7274774209bd76ad108293c
Randy Simanca <randysimancamercado@gmail.com>
2025-09-08 16:36:04 -0500

    Update datosPersonales.js

--- routes/datosPersonales.js
+++ routes/datosPersonales.js
@@ -1,10 +1,13 @@
 //backend/routes/datosPersonales.js
+
 import express from "express";
 import { crearDatosPersonales } from "../controllers/datosPersonalesControllers.js";
 import { obtenerDatosPersonales } from "../controllers/datosPersonalesControllers.js";
 import { actualizarDatosPersonales } from "../controllers/datosPersonalesControllers.js";
 import auth from "../middlewares/auth.js";
 import verificarJWT from "../middlewares/verificarJWT.js";
+
+
 
 const router = express.Router();
 
@@ -13,10 +16,11 @@ router.get("/test", (req, res) => {
   res.send("🚀 Ruta de datos-personales activa");
 });
 
-// ✅ CORRECCIÓN: Usar "/" en lugar de "/datos-personales"
-// Porque ya tienes "/api/datos-personales" en app.js
-router.post("/", verificarJWT, crearDatosPersonales);    // POST /api/datos-personales/
-router.get("/", verificarJWT, obtenerDatosPersonales);   // GET /api/datos-personales/
-router.put("/", verificarJWT, actualizarDatosPersonales); // PUT /api/datos-personales/
+// POST principal
+router.post("/datos-personales", verificarJWT, crearDatosPersonales);
+router.get("/datos-personales", verificarJWT, obtenerDatosPersonales);
+router.put("/datos-personales", verificarJWT, actualizarDatosPersonales);
+
 
 export default router;
+
